51147532d70753f54d5b2aa7c10aaf8b8b38f03a
KuldeepKumarAngural <kuldeep.navv@gmail.com>
2024-02-26 21:41:41 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import Button from "@mui/material/Button";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import { Box } from "@mui/material";
-import("prettier").Config();
+
 function App() {
   return (
   <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center" }} >
