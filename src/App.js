93017dc887a2841521011c7e2dda5cd96bace5e3
KuldeepKumarAngural <kuldeep.navv@gmail.com>
2024-02-26 21:41:00 +0530

    App-setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import * as React from "react";
+import Button from "@mui/material/Button";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import { Box } from "@mui/material";
+import("prettier").Config();
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center" }} >
+      <Button variant="contained" onClick={() => alert("Hello Developer!")}>
+        <AddCircleOutlineIcon />
+      </Button>
+    </Box>
   );
 }
 
