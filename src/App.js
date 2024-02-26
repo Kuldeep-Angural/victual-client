3f37e8f36afb2cfb43967f2af46ed3a802594271
KuldeepKumarAngural <kuldeep.navv@gmail.com>
2024-02-26 22:05:22 +0530

    prettierr configuration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import * as React from "react";
-import Button from "@mui/material/Button";
-import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
-import { Box } from "@mui/material";
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import { Box } from '@mui/material';
 
 function App() {
   return (
-  <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center" }} >
-      <Button variant="contained" onClick={() => alert("Hello Developer!")}>
+    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <Button variant="contained" onClick={() => alert('Hello Developer!')}>
         <AddCircleOutlineIcon />
       </Button>
     </Box>
